e4780eab472ce26ded4326e320fb519ab213cb66
Allen Chou <f1470891079@gmail.com>
2023-05-17 19:11:58 +0800

    calculateBooksMessage()

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,14 @@ const obj = reactive({
   nested: { count: 0 },
   arr: ['foo', 'bar']
 })
+const author = reactive({
+  name: 'John Doe',
+  books: [
+    'Vue 2 - Advanced Guide',
+    'Vue 3 - Basic Guide',
+    'Vue 4 - The Mystery'
+  ]
+})
 
 function increment() {
   count.value++
@@ -31,6 +39,12 @@ function mutateDeeply() {
   obj.nested.count++
   obj.arr.push('baz')
 }
+function calculateBooksMessage() {
+  return this.author.books.length > 0 ? 'Yes' : 'No'
+}
+function now() {
+  return Date.now()
+}
 </script>
 
 <template>
@@ -41,4 +55,8 @@ function mutateDeeply() {
   <p>Input Text: {{ text }}</p>
   <br>
   <button v-on:click="mutateDeeply">obj count is: {{ obj }}</button>
+  <br>
+  <br>
+  <p>Has published books:</p>
+  <span>{{ calculateBooksMessage() }} {{ now() }}</span>
 </template>
