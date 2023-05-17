73afcbe9cdd80615551caed140ded30a7d8deb30
Allen Chou <f1470891079@gmail.com>
2023-05-17 17:34:44 +0800

    Add vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,31 @@
+<!--
+App (root component)
+├─ TodoList
+│  └─ TodoItem
+│     ├─ TodoDeleteButton
+│     └─ TodoEditButton
+└─ TodoFooter
+   ├─ TodoClearButton
+   └─ TodoStatistics
+-->
 <script setup>
 import { ref } from 'vue'
 
 const count = ref(0)
+const text = ref('')
 
 function increment() {
   count.value++
+}
+function onInput(e) {
+  text.value = e.target.value
 }
 </script>
 
 <template>
   <button v-on:click="increment">count is: {{ count }}</button>
+  <br>
+  <br>
+  <input :value="text" @input="onInput" placeholder="Type here">
+  <p>Input Text: {{ text }}</p>
 </template>
