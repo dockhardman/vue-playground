2709b12d393fe60685b501a1a88142fcf0e55830
Allen Chou <f1470891079@gmail.com>
2023-05-17 18:45:44 +0800

    reactive.

--- src/App.vue
+++ src/App.vue
@@ -9,16 +9,27 @@ App (root component)
    └─ TodoStatistics
 -->
 <script setup>
-import { ref } from 'vue'
+import { nextTick, ref, reactive } from 'vue'
 
 const count = ref(0)
 const text = ref('')
+const obj = reactive({
+  nested: { count: 0 },
+  arr: ['foo', 'bar']
+})
 
 function increment() {
   count.value++
+  nextTick(() => {
+    // access updated DOM
+  })
 }
 function onInput(e) {
   text.value = e.target.value
+}
+function mutateDeeply() {
+  obj.nested.count++
+  obj.arr.push('baz')
 }
 </script>
 
@@ -28,4 +39,6 @@ function onInput(e) {
   <br>
   <input :value="text" @input="onInput" placeholder="Type here">
   <p>Input Text: {{ text }}</p>
+  <br>
+  <button v-on:click="mutateDeeply">obj count is: {{ obj }}</button>
 </template>
